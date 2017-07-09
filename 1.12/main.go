3a3a6677c561650d8aa9b4f0d33e4cec56cb1d59
advoretskiy80@gmail.com <advoretskiy80@gmail.com>
2017-07-08 17:23:30 -0700

    Solved ex 1.12

--- 1.12/main.go
+++ 1.12/main.go
@@ -18,7 +18,8 @@ import (
 	"math/rand"
 	"fmt"
 	"net/url"
-	//"strconv"
+	"strconv"
+	"reflect"
 )
 //https://golang.org/pkg/net/url/#Values
 var palette = []color.Color{color.White, color.Black}
@@ -33,7 +34,7 @@ func main() {
 			fmt.Println(r.URL.RawQuery)
     			m, _ := url.ParseQuery(r.URL.RawQuery)
     			fmt.Println(m)
-			/*cycles, err := strconv.ParseFloat(m["cycles"][0], 64)
+			cycles, err := strconv.Atoi(m["cycles"][0])
     			check(err)
 			res, err := strconv.ParseFloat(m["res"][0], 64)
     			check(err)
@@ -42,38 +43,33 @@ func main() {
 			nframes, err := strconv.Atoi(m["nframes"][0])
     			check(err)
 			delay, err := strconv.Atoi(m["delay"][0])
-    			check(err)*/
-			lissajous(w/*, cycles, res, size, nframes, delay*/)
+    			check(err)
+			/*const (
+				cycles  = 5     // number of complete x oscillator revolutions
+				res     = 0.001 // angular resolution
+				size    = 100   // image canvas covers [-size..+size]
+				nframes = 64    // number of animation frames
+				delay   = 8     // delay between frames in 10ms units
+			)*/
+			lissajous(w, cycles, res, size, nframes, delay)
 	}
 	http.HandleFunc("/", handler) // each request calls handler
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func lissajous(out io.Writer/*, cycles int, res float64, size int, nframes int, delay int*/) {
-	/*const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)*/
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
+func lissajous(out io.Writer, cycles int, res float64, size int, nframes int, delay int) {
+	fmt.Println(reflect.TypeOf(cycles))//debugging, something wrong with types
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+
+		for t := 0.0; t < float64(cycles)*2.0*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5),
 				blackIndex)
 		}
 		phase += 0.1
