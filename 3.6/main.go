8f719ecf34cba15a5a9019d5512e7c3a9b636999
advoretskiy80@gmail.com <advoretskiy80@gmail.com>
2017-08-19 20:01:06 -0700

    implemented supersampling. Solved ex 3.6

--- 3.6/main.go
+++ 3.6/main.go
@@ -12,9 +12,10 @@ import (
 	"image/color"
 	"image/png"
 	"math/cmplx"
-	"net/http"
+	"math"
+	//"net/http"
 	"log"
-	"fmt"
+	"os"
 )
 
 func main() {
@@ -22,37 +23,53 @@ func main() {
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
 		width, height          = 1024, 1024
 	)
+	widthP, heightP := width, height
 
-	widthP := width*2
-	heightP := width*2
-
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		img := image.NewRGBA(image.Rect(0, 0, width, height))
-		for py := 0; py < heightP; py = py+2 {
-			y1 := float64(py)/float64(heightP*(ymax-ymin) + ymin)
-			//y2 := float64(py+1)/float64(heightP*(ymax-ymin) + ymin)
-			for px := 0; px < widthP; px = px+2 {
-				x1 := float64(px)/float64(widthP*(xmax-xmin) + xmin)
-				//x2 := float64(px+1)/float64(widthP*(xmax-xmin) + xmin)
-				z11 := complex(x1, y1)
-				//z12 := complex(x1, y2)
-				//z21 := complex(x2, y1)
-				//z22 := complex(x2, y2)
-				// Image point (px, py) represents complex value z.
-				img.Set(px, py, mandelbrot(z11))
-				//c := mandelbrot(z11)
-				//Y, Cb, Cr
-				fmt.Printf("%v\n",z11)
-			}
-		}
-		
-		if err := png.Encode(w, img); err != nil {
-			log.Fatal(err)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < heightP; py++ {
+		y1 := float64(py)/float64(widthP)*(ymax-ymin) + ymin
+		y2 := float64(py+1)/float64(heightP)*(ymax-ymin) + ymin
+		for px := 0; px < widthP; px++ {
+			x1 := float64(px)/float64(widthP)*(xmax-xmin) + xmin
+			x2 := float64(px+1)/float64(heightP)*(xmax-xmin) + xmin
+			z11 := complex(x1, y1)
+			z12 := complex(x1, y2)
+			z21 := complex(x2, y1)
+			z22 := complex(x2, y2)
+			// Image point (px, py) represents complex value z.
+			c1 := mandelbrot(z11)
+			c2 := mandelbrot(z12)
+			c3 := mandelbrot(z21)
+			c4 := mandelbrot(z22)
+			// Averaging colors
+			r1,g1,b1,_ := c1.RGBA()
+			r2,g2,b2,_ := c2.RGBA()
+			r3,g3,b3,_ := c3.RGBA()
+			r4,g4,b4,_ := c4.RGBA()
+			r := (r1+r2+r3+r4)/4
+			g := (g1+g2+g3+g4)/4
+			b := (b1+b2+b3+b4)/4
+			ru := r >> 8
+			gu := g >> 8
+			bu := b >> 8
+			c := color.RGBA{uint8(ru), uint8(gu), uint8(bu), 255}
+			img.Set(px, py, c)
 		}
 	}
-
-	http.HandleFunc("/", handler) // each request calls handler
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	
+	f, err := os.Create("image.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	if err := png.Encode(f, img); err != nil {
+		f.Close()
+		log.Fatal(err)
+	}
+	
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
 
 }
 
@@ -64,9 +81,10 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
+			return acosRGBA(v)
 			//return acos(v)
 			//return sqrt(v)
-			return newton(v)
+			//return newton(v)
 		}
 	}
 	return color.Black
@@ -75,6 +93,12 @@ func mandelbrot(z complex128) color.Color {
 //!-
 
 // Some other interesting functions:
+func acosRGBA(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.RGBA{red, uint8(math.Abs(float64(red-blue))), blue, 255}
+}
 
 func acos(z complex128) color.Color {
 	v := cmplx.Acos(z)
