e06c7016e794178f1be858da592743ba180529bf
Julineo <advoretskiy80@gmail.com>
2017-09-20 15:58:22 -0700

    Solved 3.12

--- 3.13/main.go
+++ 3.13/main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	)
 
-const 
-( 
-_ = 1 << (10 * iota) 
-KiB // 1024 
-MiB // 1048576 
-GiB // 1073741824 
-TiB // 1099511627776 (exceeds 1 << 32) 
-PiB // 1125899906842624 
-EiB // 1152921504606846976 
-ZiB // 1180591620717411303424 (exceeds 1 << 64) 
-YiB // 1208925819614629174706176 
-)
+const ( KB, MB, GB, TB, PB, EB, ZB, YB = 1e3, 1e6, 1e9, 1e12, 1e15, 1e18, 1e21, 1e24 )
 
 func main() {
-	fmt.Println(KiB)
+	fmt.Printf("%b", MB)
 }
