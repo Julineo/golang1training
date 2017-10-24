396c34407172ff0648c0496302eae0c6f571d0ae
advoretskiy80@gmail.com <advoretskiy80@gmail.com>
2017-10-23 21:04:29 -0700

    Solved ex.4.9

--- 4.9/main.go
+++ 4.9/main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
-	//"io"
 	"os"
-	//"unicode"
-	"strings"
 )
 
 func main() {
-	//countsWords := make(map[string]int)
+	countsWords := make(map[string]int)
 
-	scanner := bufio.NewScanner(strings.NewReader(os.Stdin))
+	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text)
+		countsWords[scanner.Text()]++
+	}
+	for c, n := range countsWords {
+		fmt.Printf("%q\t%d\n", c, n)
 	}
 }
