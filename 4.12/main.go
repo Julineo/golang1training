047b9d8726931083d54b50294a73eb2adb93d053
Julineo <advoretskiy80@gmail.com>
2018-01-29 17:03:48 -0800

    Worked on ex 4.12

--- 4.12/main.go
+++ 4.12/main.go
@@ -5,9 +5,15 @@ import (
 	"net/http"
 	"fmt"
 	"encoding/json"
+	"strconv"
 )
 
-const RequestURL = "https://xkcd.com/571/info.0.json"
+//We'll use this const for figuring out the max number of comics.
+const RequestLastURL = "https://xkcd.com/info.0.json"
+
+type ResponseResultLast struct {
+	Num int
+}
 
 type ResponseResult struct {
 	Month string
@@ -19,17 +25,38 @@ func main() {
 
 	//q := url.QueryEscape(strings.Join(terms, " "))
 	//resp, err := http.Get(IssuesURL + "?q=" + q)
-	resp, err := http.Get(RequestURL)
+	resp, err := http.Get(RequestLastURL)
 	if err != nil {
 		fmt.Errorf("Error : %s", err)
 	}
-	
+		
+	var resultLast ResponseResultLast
 	var result ResponseResult
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&resultLast); err != nil {
 		resp.Body.Close()
 		fmt.Errorf("Error : %s", err)
 	}
 	resp.Body.Close()
-	fmt.Println(result)
+	
+	var ResponseResults[]ResponseResult
+	//Loop through all comics
+	for i := 1; i <= resultLast.Num; i++ {
+		RequestURL := "https://xkcd.com/" + strconv.FormatInt(int64(i), 10) + "/info.0.json"
+		
+		resp, err := http.Get(RequestURL)
+		if err != nil {
+			fmt.Errorf("Error: %s", err)
+		}
+		
+		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+			resp.Body.Close()
+			fmt.Errorf("Error : %s", err)
+		}
+		resp.Body.Close()
+		ResponseResults = append(ResponseResults, result)
+		
+		fmt.Println(RequestURL)
+	}
+	fmt.Println(ResponseResults)
 
 }
