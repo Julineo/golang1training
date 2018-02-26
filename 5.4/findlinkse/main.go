833a493b3d648f9475c604bf96bb125715f114f4
advoretskiy80@gmail.com <advoretskiy80@gmail.com>
2018-02-25 17:33:12 -0800

    added images to visit function

---
+++ 5.4/findlinkse/main.go
@@ -0,0 +1,73 @@
+// Extend the visit function so that it extracts other kinds of links from the document, such as images, scripts, and style sheets.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(nil, doc)
+}
+
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+				fmt.Println(links)
+			}
+		}
+	}
+	if n.Type == html.ElementNode && (n.Data == "img" || n.Data == "script") {
+		for _, a := range n.Attr {
+			if a.Key == "src" {
+				links = append(links, a.Val)
+				fmt.Println(links)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(links, c)
+	}
+}
+
+//!-visit
+
+/*
+//!+html
+package html
+
+type Node struct {
+	Type                    NodeType
+	Data                    string
+	Attr                    []Attribute
+	FirstChild, NextSibling *Node
+}
+
+type NodeType int32
+
+const (
+	ErrorNode NodeType = iota
+	TextNode
+	DocumentNode
+	ElementNode
+	CommentNode
+	DoctypeNode
+)
+
+type Attribute struct {
+	Key, Val string
+}
+
+func Parse(r io.Reader) (*Node, error)
+//!-html
+*/
