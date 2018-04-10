4f0ac9cc74b0c697e956599bcc94921b2d638078
Julineo <advoretskiy80@gmail.com>
2018-04-09 17:08:45 -0700

    worked on 5.7

--- 5.7/outline.go
+++ 5.7/outline.go
@@ -34,9 +34,11 @@ func outline(url string) error {
 
 // forEachNode prints html outline in pretty form
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+
 	if pre != nil {
 		pre(n)
 	}
+	fmt.Println(n.FirstChild)
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		forEachNode(c, pre, post)
@@ -51,7 +53,10 @@ var depth int
 
 //modyfied version of startElement
 func startElement(n *html.Node) {
-	if n.Type == html.ElementNode {
+	if n.Type == html.ElementNode && n.FirstChild == nil {
+		fmt.Printf("%*s<%s/>\n", depth*2, "", n.Data)
+	}
+	if n.Type == html.ElementNode && n.FirstChild != nil {
 		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
 		depth++
 	}
@@ -62,7 +67,7 @@ func startElement(n *html.Node) {
 
 //modyfied version of endElement
 func endElement(n *html.Node) {
-	if n.Type == html.ElementNode {
+	if n.Type == html.ElementNode && n.FirstChild != nil {
 		depth--
 		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
 	}
