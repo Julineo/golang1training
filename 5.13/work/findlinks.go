a6107245ee77b5660303b84add8641884fb86df1
Julineo <advoretskiy80@gmail.com>
2018-05-21 16:51:29 -0700

    worked

--- 5.13/work/findlinks.go
+++ 5.13/work/findlinks.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"io"
 	"strings"
+	"path/filepath"
 
 	"gopl.io/ch5/links"
 )
@@ -61,9 +62,10 @@ func crawl(urla string) []string {
 	if ! flag {
 		return []string{}
 	}
-	fmt.Println(urla, path.Base(urla))
+	fmt.Println(urla)
 	fmt.Println(path.Base(urla))
-	//download(urla, path.Base(urla))
+
+	download(urla)
 
 
 	list, err := links.Extract(urla)
@@ -73,21 +75,52 @@ func crawl(urla string) []string {
 	return list
 }
 
-//download url to filename
-func download(url, filename string) {
-	resp, err := http.Get(url)
+var origHost string
+
+// Download url to filename
+func download(rawurl string) error {
+	url, err := url.Parse(rawurl)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("bad url: %s", err)
+	}
+	if origHost == "" {
+		origHost = url.Host
+	}
+	if origHost != url.Host {
+		return nil
+	}
+	dir := url.Host
+	var filename string
+	if filepath.Ext(filename) == "" {
+		dir = filepath.Join(dir, url.Path)
+		filename = filepath.Join(dir, "index.html")
+	} else {
+		dir = filepath.Join(dir, filepath.Dir(url.Path))
+		filename = url.Path
+	}
+	err = os.MkdirAll(dir, 0777)
+	if err != nil {
+		return err
+	}
+	resp, err := http.Get(rawurl)
+	if err != nil {
+		return err
 	}
 	defer resp.Body.Close()
-
-	f, err := os.Create(filename)
+	file, err := os.Create(filename)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	defer f.Close()
-
-	io.Copy(f, resp.Body)
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+	// Check for delayed write errors section 5.8.
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
