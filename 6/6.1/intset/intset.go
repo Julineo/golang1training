7c7c54d0d73af9db8350da3d16c2b6bc68a8a134
Julineo <advoretskiy80@gmail.com>
2018-06-07 16:37:00 -0700

    solved 6.1

--- 6/6.1/intset/intset.go
+++ 6/6.1/intset/intset.go
@@ -30,20 +30,10 @@ func (s *IntSet) Has(x int) bool {
 // Add adds the non-negative value x to the set.
 func (s *IntSet) Add(x int) {
 	word, bit := x/64, uint(x%64)
-	fmt.Println("word: ", word)
-	fmt.Printf("%8b\n", word)
-	fmt.Println("bit: ", bit)
-	fmt.Printf("%8b\n", bit)
-
 	for word >= len(s.words) {
-		fmt.Println(s.words)
 		s.words = append(s.words, 0)
-		fmt.Println(s.words)
 	}
-	fmt.Println("1 << bit:", 1 << bit)
 	s.words[word] |= 1 << bit
-	fmt.Println("s.words[word]: ", s.words[word])
-	fmt.Printf("%8b\n", s.words[word])
 }
 
 // UnionWith sets s to the union of s and t.
@@ -78,7 +68,7 @@ func (s *IntSet) String() string {
 	return buf.String()
 }
 
-// returns the number of elements
+// Returns the number of elements
 func (s *IntSet) Len() (count int) {
 	for _, word := range s.words {
 		count += popcount(word)
@@ -86,11 +76,31 @@ func (s *IntSet) Len() (count int) {
 	return
 }
 
-// returns number of 1 bits
+// Returns number of 1 bits
 func popcount(x uint64) (count int) {
 	for x != 0 {
 		count++
 		x &= x - 1
 	}
 	return
+}
+
+// Remove x from the set
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	s.words[word] -= 1 << bit
+}
+
+ // Remove all elements from the set
+func (s *IntSet) Clear() {
+	s.words = []uint64{}
+}
+
+// Return a copy of the set
+func (s *IntSet) Copy() *IntSet {
+	var ret IntSet
+	tmp := make([]uint64, len(s.words))
+	copy(tmp, s.words)
+	ret.words = tmp
+	return &ret
 }
--- 6/6.1/main.go
+++ 6/6.1/main.go
@@ -6,11 +6,32 @@ import (
 )
 
 func main() {
-	var x IntSet
+	var x, y IntSet
+	x.Add(0)
 	x.Add(1)
-	x.Add(64)
-	x.Add(500)
+	x.Add(2)
+	x.Add(3)
+	x.Add(4)
+	x.Add(5)
+	x.Add(6)
+	x.Add(7)
 	fmt.Println(x)
 	fmt.Println(x.String())
+
 	fmt.Println(x.Len())
+
+	x.Remove(2)
+	fmt.Println(x)
+	fmt.Println(x.String())
+
+	fmt.Println("Copy: ", x.Copy())
+	y = *x.Copy()
+
+	x.Clear()
+	fmt.Println(x)
+	fmt.Println(x.String())
+	x.Add(1)
+
+	fmt.Println(x.String())
+	fmt.Println(y.String())
 }
