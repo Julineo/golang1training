0dbe762fc597b29f6e6c2fd3a2f8fde5b76ce4b6
Julineo <advoretskiy80@gmail.com>
2018-06-11 16:20:07 -0700

    edited comments

--- 6/6.5/intset/intset.go
+++ 6/6.5/intset/intset.go
@@ -1,5 +1,5 @@
 /*
-ex 6.4: Add a method Elems that returns a slice containing the elements of the set, suitable for iterating over with a range loop.
+ex 6.5: The type of each word used by IntSet is uint64, but 64-bit arithmetic may be inefficient on a 32-bit platform. Modify the program to use the uint type, which is the most efficient unsigned integer type for the platform. Instead of dividing by 64, define a constant holding the effective size of uint in bits, 32 or 64
 */
 
 // Package intset provides a set of integers based on a bit vector.
