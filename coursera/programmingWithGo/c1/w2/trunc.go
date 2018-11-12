330e513254f27a9da23d75335247a1489abb4b68
Julineo <advoretskiy80@gmail.com>
2018-11-12 11:02:04 -0800

    solved task

---
+++ coursera/programmingWithGo/c1/w2/trunc.go
@@ -0,0 +1,25 @@
+/*
+Write a program which prompts the user to enter a floating point number and prints the integer which is a truncated version of the floating point number that was entered. Truncation is the process of removing the digits to the right of the decimal place.
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"log"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter float: ")
+	str, _ := reader.ReadString('\n')
+
+	_, err := strconv.ParseFloat(str[:len(str)-1], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(strings.Split(str, ".")[0])
+}
