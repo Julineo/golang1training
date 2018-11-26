c39e6ff259421250f61cb58865bb76ec64523e1a
Julineo <advoretskiy80@gmail.com>
2018-11-26 08:02:15 -0800

    worked on philo problem

---
+++ coursera/programmingWithGo/c3/w4/phil.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type chopS struct{ sync.Mutex }
+
+type philo struct {
+	leftCS, rightCS *chopS
+	number          int
+}
+
+func (p philo) eat() {
+	for i := 0; i < 3; i++ {
+		p.leftCS.Lock()
+		p.rightCS.Lock()
+
+		fmt.Println("starting to eat", p.number)
+		fmt.Println(time.Now())
+		fmt.Println("finishing to eat", p.number)
+
+		p.rightCS.Unlock()
+		p.leftCS.Unlock()
+	}
+}
+
+func main() {
+	cSticks := make([]*chopS, 5)
+	for i := 0; i < 5; i++ {
+		cSticks[i] = new(chopS)
+	}
+
+	philos := make([]*philo, 5)
+	for i := 0; i < 5; i++ {
+		fmt.Println(i, (i+1)%5)
+		philos[i] = &philo{cSticks[i], cSticks[(i+1)%5], i}
+	}
+
+	for i := 0; i < 5; i++ {
+		go philos[i].eat()
+	}
+
+	time.Sleep(3 * time.Second)
+}
