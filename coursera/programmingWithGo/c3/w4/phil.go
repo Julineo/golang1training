f95da29ce52ac8932d996304f86950c13dd5f406
Julineo <advoretskiy80@gmail.com>
2018-11-28 12:18:19 -0800

    moved host to separate goroutine

--- coursera/programmingWithGo/c3/w4/phil.go
+++ coursera/programmingWithGo/c3/w4/phil.go
@@ -11,25 +11,40 @@ type chopS struct{ sync.Mutex }
 type philo struct {
 	leftCS, rightCS *chopS
 	number          int
+	permis chan int
+	done chan int
 }
 
-func (p philo) eat() {
+func (p *philo) eat() {
 	for i := 0; i < 3; i++ {
+		permis := <-p.permis
 		p.leftCS.Lock()
 		p.rightCS.Lock()
 
 		fmt.Println("starting to eat", p.number)
 		fmt.Println(time.Now())
+		time.Sleep(1 * time.Second)
 		fmt.Println("finishing to eat", p.number)
 
 		p.rightCS.Unlock()
 		p.leftCS.Unlock()
+		p.done <- permis
+	}
+}
+
+// limits number of eating philos
+func host(permis chan int, done chan int) {
+	permis <- 1
+	permis <- 1
+
+	for {
+		permis <- <-done
 	}
 }
 
 func main() {
-	maxGoroutines := 2
-	host := make(chan struct{}, maxGoroutines)
+	var permis = make(chan int, 2)
+	var done = make(chan int, 2)
 
 	cSticks := make([]*chopS, 5)
 	for i := 0; i < 5; i++ {
@@ -39,14 +54,13 @@ func main() {
 	philos := make([]*philo, 5)
 	for i := 0; i < 5; i++ {
 		fmt.Println(i, (i+1)%5)
-		philos[i] = &philo{cSticks[i], cSticks[(i+1)%5], i}
+		philos[i] = &philo{cSticks[i], cSticks[(i+1)%5], i, permis, done}
 	}
 
 	for i := 0; i < 5; i++ {
-		host <- struct{}{}
 		go philos[i].eat()
-		<-host
 	}
+	go host(permis, done)
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(10 * time.Second)
 }
